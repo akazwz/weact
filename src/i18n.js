fbefe6edb8481b824841334ff0ae0fcf856b55c8
akez <1791189210@qq.com>
2021-07-16 17:41:33 +0800

    change i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
-import Backend from 'i18next-http-backend';
+// import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import TranslationZH from '/public/zh_translation.json';
 import TranslationEN from '/public/en_translation.json';
@@ -16,7 +16,7 @@ const resources = {
 
 i18n
     .use(initReactI18next)
-    .use(Backend)
+    //.use(Backend)
     .use(LanguageDetector)
     .init({
         resources,
