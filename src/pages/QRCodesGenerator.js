070e60adc9add6b8a0ac91d30b0810d47c6c901b
akez <1791189210@qq.com>
2021-07-16 11:07:47 +0800

    add container

--- src/pages/QRCodesGenerator.js
+++ src/pages/QRCodesGenerator.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
 
 function QRCodesGenerator() {
     return (
-        <div>
-            <h1>this is qr codes generator</h1>
-        </div>
+        <Fragment>
+            <Container maxWidth={"sm"}>
+                <CssBaseline/>
+                <Typography>
+                    <h1>this is qr generator</h1>
+                </Typography>
+            </Container>
+        </Fragment>
     );
 }
 
