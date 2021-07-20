51d30d60ed6c0226ff7e8b3606aa645edc7fe8c5
akazwz <akaedison@icloud.com>
2021-07-20 21:34:27 +0800

    change api

--- src/api/file.js
+++ src/api/file.js
@@ -1,5 +1,5 @@
-const axios = require('axios').default
-const baseUrl = 'http://localhost:8000';
+const axios = require('axios').default;
+const baseUrl = 'http://hellozwz:8000';
 const fileUpload = async (formData) => {
     return await axios.post(baseUrl + '/file', formData);
 }
